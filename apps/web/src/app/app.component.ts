41906ecd9294b4e1514bab5e61b37f4956af4ce9
Alexander Edemskiy <edemskiy@ya.ru>
2023-03-04 17:25:50 +0300

    feat: eslint + prefix change

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,10 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
 
 @Component({
-  selector: 'diplom-root',
+  selector: 'sf-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   constructor(private readonly http: HttpClient) {
