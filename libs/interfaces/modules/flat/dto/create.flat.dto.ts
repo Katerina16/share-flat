e8697033e4afcbdf0537834f25a4a5beb967e939
Ekaterina Kharkova <eh@zaoblako.ru>
2023-03-26 17:32:08 +0300

    create flat dto

---
+++ libs/interfaces/modules/flat/dto/create.flat.dto.ts
@@ -0,0 +1,74 @@
+import {IsNotEmpty, IsOptional} from "class-validator";
+import {FlatEntity} from "@sf/interfaces/modules/flat/entities/flat.entity";
+import {PropertyValueEntity} from "@sf/interfaces/modules/flat/entities/property.value.entity";
+
+export class CreateFlatDto implements Partial<FlatEntity> {
+  @IsNotEmpty({
+    message: 'Не заполнено поле $property'
+  })
+  name: string;
+
+  @IsNotEmpty({
+    message: 'Не заполнено поле $property'
+  })
+  address: string;
+
+  @IsNotEmpty({
+    message: 'Не заполнено поле $property'
+  })
+  latitude: number;
+
+  @IsNotEmpty({
+    message: 'Не заполнено поле $property'
+  })
+  longitude: number;
+
+  @IsNotEmpty({
+    message: 'Не заполнено поле $property'
+  })
+  square: number;
+
+  @IsNotEmpty({
+    message: 'Не заполнено поле $property'
+  })
+  floor: number;
+
+  @IsNotEmpty({
+    message: 'Не заполнено поле $property'
+  })
+  floors: number;
+
+  @IsNotEmpty({
+    message: 'Не заполнено поле $property'
+  })
+  description: string;
+
+  @IsNotEmpty({
+    message: 'Не заполнено поле $property'
+  })
+  price: number;
+
+  @IsOptional()
+  shared: boolean;
+
+  @IsNotEmpty({
+    message: 'Не заполнено поле $property'
+  })
+  rooms: number;
+
+  @IsNotEmpty({
+    message: 'Не заполнено поле $property'
+  })
+  guests: number;
+
+  @IsNotEmpty({
+    message: 'Не заполнено поле $property'
+  })
+  photos: string[];
+
+  @IsNotEmpty({
+    message: 'Не заполнено поле $property'
+  })
+  propertyValues: PropertyValueEntity[];
+
+}
